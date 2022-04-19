8cabcaa742d8d8a18f64e2904df259984383f82f
Administrator <@xtz-70050-17>
2022-04-19 13:17:48 +0300

    Выполнил дз

---
+++ src/main/java/org/example/dao/FoodDaoListKeeper.java
@@ -0,0 +1,64 @@
+package org.example.dao;
+
+import org.example.entity.FoodEntity;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FoodDaoListKeeper implements FoodDao {
+    private final List<FoodEntity> foodEntityKeeper = new ArrayList<>();
+
+    @Override
+    public void create(FoodEntity foodEntity) {
+        if (foodEntityKeeper.stream()
+                .anyMatch(e -> e.getId().equals(foodEntity.getId())))
+            System.out.println("Id already used, please use the function 'update'");
+        else foodEntityKeeper.add(foodEntity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        foodEntityKeeper.removeIf(e -> e.getId().equals(id));
+
+    }
+
+    @Override
+    public void deleteByName(String name) {
+        foodEntityKeeper.removeIf(e -> e.getName().equals(name));
+
+    }
+
+    @Override
+    public void update(FoodEntity foodEntity) {
+        if (foodEntityKeeper.contains(foodEntity)) {
+            foodEntityKeeper.removeIf(e -> e.getId().equals(foodEntity.getId()));
+            foodEntityKeeper.add(foodEntity);
+        }
+
+    }
+
+    @Override
+    public List<FoodEntity> findAll() {
+        return new ArrayList<>(foodEntityKeeper);
+    }
+
+    @Override
+    public FoodEntity findById(Long id) {
+        return foodEntityKeeper.stream()
+                .filter(e -> e.getId().equals(id))
+                .findFirst()
+                .get();
+    }
+
+    @Override
+    public List<FoodEntity> findByName(String name) {
+        return foodEntityKeeper.stream()
+                .filter(e -> e.getName().equals(name))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void truncate() {
+        foodEntityKeeper.clear();
+    }
+}
---
+++ src/main/java/org/example/dao/Test.java
@@ -0,0 +1,29 @@
+package org.example.dao;
+
+import org.example.csv_worker.IllegalFileExtensionException;
+import org.example.entity.FoodEntity;
+
+import java.io.IOException;
+
+public class Test {
+    public static void main(String[] args) throws IllegalFileExtensionException, IOException {
+        FoodDaoListKeeper foodDaoListKeeper = new FoodDaoListKeeper();
+
+
+        foodDaoListKeeper.create(new FoodEntity(1L, "banana"));
+        foodDaoListKeeper.create(new FoodEntity(2L, "anana"));
+        foodDaoListKeeper.create(new FoodEntity(3L, "ana"));
+        foodDaoListKeeper.create(new FoodEntity(4L, "apple"));
+        foodDaoListKeeper.create(new FoodEntity(4L, "ape"));
+
+        System.out.println(foodDaoListKeeper.findById(4L));
+        System.out.println(foodDaoListKeeper.findByName("ana"));
+
+        foodDaoListKeeper.update(new FoodEntity(4L, "pineapple"));
+        foodDaoListKeeper.deleteById(2L);
+        foodDaoListKeeper.deleteByName("ana");
+        System.out.println(foodDaoListKeeper.findAll().toString());
+        foodDaoListKeeper.truncate();
+        System.out.println(foodDaoListKeeper.findAll().toString());
+    }
+}
