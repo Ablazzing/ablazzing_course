a0c33f2a316f506a55fe65ad4ea45f253b34ffde
Dmitrii But <voodooman88@gmail.com>
2022-04-19 22:56:38 +0700

    (ДЗ) реализовал методы интерфейса FoodDao для работы с Arraylist

---
+++ src/main/java/org/example/services/Test.java
@@ -0,0 +1,42 @@
+package org.example.services;
+
+import org.example.entity.FoodEntity;
+
+public class Test {
+    public static void main(String[] args) {
+        FoodServiceImplementation foodServiceImplementationObject = new FoodServiceImplementation();
+//        foodServiceImplementationObject.create(new FoodEntity(1L, "banana"));
+//        foodServiceImplementationObject.create(new FoodEntity(2L, "banana"));
+//        foodServiceImplementationObject.create(new FoodEntity(3L, "banana"));
+//        foodServiceImplementationObject.create(new FoodEntity(4L, "banana"));
+//        foodServiceImplementationObject.create(new FoodEntity(5L, "banana"));
+//        foodServiceImplementationObject.create(new FoodEntity(6L, "banana"));
+//        foodServiceImplementationObject.create(new FoodEntity(7L, "banana"));
+//        foodServiceImplementationObject.create(new FoodEntity(8L, "banana"));
+//        foodServiceImplementationObject.create(new FoodEntity(9L, "banana"));
+//        foodServiceImplementationObject.create(new FoodEntity(10L, "banana"));
+//        foodServiceImplementationObject.findAll();
+//        foodServiceImplementationObject.findById(5L);
+//        foodServiceImplementationObject.update(new FoodEntity(5L, "potato"));
+//        foodServiceImplementationObject.findById(5L);
+//        foodServiceImplementationObject.deleteById(1L);
+//        foodServiceImplementationObject.deleteByName("banana");
+//        foodServiceImplementationObject.deleteByName("banana");
+//        foodServiceImplementationObject.deleteByName("banana");
+//        foodServiceImplementationObject.deleteByName("banana");
+//        foodServiceImplementationObject.deleteByName("banana");
+//        foodServiceImplementationObject.deleteByName("banana");
+//        foodServiceImplementationObject.deleteByName("banana");
+//        foodServiceImplementationObject.deleteByName("banana");
+//        foodServiceImplementationObject.deleteByName("banana");
+//        foodServiceImplementationObject.deleteByName("banana");
+//        foodServiceImplementationObject.getFoodList();
+//        foodServiceImplementationObject.findAll();
+//        foodServiceImplementationObject.deleteByName("strawberry");
+//        foodServiceImplementationObject.deleteByName("banana");
+//        foodServiceImplementationObject.findById(1L);
+
+
+    }
+
+}
