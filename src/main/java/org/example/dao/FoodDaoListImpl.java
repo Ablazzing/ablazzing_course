153ce179f1b31f8ba76c771e21e7ce1173ccbbfe
Kambulov Ivan <kia289@mail.ru>
2022-04-21 18:26:29 +0300

    Добавлена имплементация интерфейса FoodDao

---
+++ src/main/java/org/example/dao/FoodDaoListImpl.java
@@ -0,0 +1,80 @@
+package org.example.dao;
+
+import org.example.csv_worker.IllegalFileExtensionException;
+import org.example.entity.FoodEntity;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FoodDaoListImpl implements FoodDao {
+    private List<FoodEntity> foodList = new ArrayList<>();
+    private long currentId = 1L;
+
+    @Override
+    public void create(FoodEntity foodEntity) throws IllegalFileExtensionException, IOException {
+        foodEntity.setId(currentId);
+        foodList.add(foodEntity);
+        currentId++;
+    }
+
+    @Override
+    public void deleteById(Long id) throws IllegalFileExtensionException, IOException {
+        foodList = foodList.stream()
+                .filter(e -> !e.getId().equals(id))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteByName(String name) throws IllegalFileExtensionException, IOException {
+        foodList = foodList.stream()
+                .filter(e -> !e.getName().equals(name))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void update(FoodEntity foodEntity) throws IllegalFileExtensionException, IOException {
+        if (foodEntity.getId() != null) {
+            for (FoodEntity f : foodList) {
+                if (f.getId().equals(foodEntity.getId())) {
+                    f.setName(foodEntity.getName());
+                }
+            }
+        }
+    }
+
+    @Override
+    public List<FoodEntity> findAll() throws IllegalFileExtensionException, IOException {
+        return foodList;
+    }
+
+    @Override
+    public FoodEntity findById(Long id) throws IllegalFileExtensionException, IOException {
+        for (FoodEntity f : foodList) {
+            if (f.getId().equals(id)) {
+                return f;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<FoodEntity> findByName(String name) throws IllegalFileExtensionException, IOException {
+        return foodList.stream()
+                .filter(e -> e.getName().equals(name))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void truncate() throws IllegalFileExtensionException, IOException {
+        foodList.clear();
+    }
+
+    @Override
+    public String toString() {
+        return "FoodDaoListImpl{" +
+                "foodList=" + foodList +
+                '}';
+    }
+}
